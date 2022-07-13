1372bf37faae42e331cdbf17d852e94039e2d55d
scaabel <scaabel@users.noreply.github.com>
2022-07-13 08:49:56 +0000

    Fix styling

--- src/Models/State.php
+++ src/Models/State.php
@@ -3,7 +3,6 @@
 namespace Kenyalang\Countries\Models;
 
 use Illuminate\Database\Eloquent\Builder;
-use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Carbon;
 use Kenyalang\Countries\Traits\HasCountry;
