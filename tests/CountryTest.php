252ebeba0437589a1cb1a0c97dbe3f16735ea7a7
Scalia Abel <ariev@viprewards.com.my>
2022-07-13 16:23:11 +0800

    test: add unit tests

---
+++ tests/CountryTest.php
@@ -0,0 +1,40 @@
+<?php
+
+use Kenyalang\Countries\Models\Country;
+use Kenyalang\Countries\Models\State;
+use Kenyalang\Countries\Enums\CountryStatus;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+uses(RefreshDatabase::class);
+
+test('it should populate database with countries', function () {
+    $this->artisan('migrate');
+
+    $country = Country::whereName('Malaysia')->first();
+
+    $state = $country->states()->where('name', 'Johor')->first();
+
+    expect($country->name)->toBe('Malaysia')
+        ->and($state->name)->toBe('Johor');
+});
+
+test('it should only get active countries', function () {
+    $this->artisan('migrate');
+
+    $country = Country::whereName('Malaysia')->first();
+
+    $country->update(['status' => CountryStatus::ACTIVE->value]);
+
+    $countries = Country::active()->get();
+
+    expect($countries->count())->toBe(1)
+        ->and($countries->first()->name)->toBe($country->name);
+});
+
+test('it can get states with locale', function () {
+    $this->artisan('migrate');
+
+    $state = State::withLocale()->whereName('Johor')->first();
+
+    expect($state)->toHaveKeys(['currency_code', 'currency_name', 'currency_symbol', 'timezone', 'phone_code']);
+});
