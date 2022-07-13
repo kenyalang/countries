a1aeeda5490cf49f58ac58873d0779062bf43b7a
Scalia Abel <ariev@viprewards.com.my>
2022-07-13 16:49:31 +0800

    chore: add `docblocks` to `models` and `traits`

--- src/Models/Country.php
+++ src/Models/Country.php
@@ -2,10 +2,49 @@
 
 namespace Kenyalang\Countries\Models;
 
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Support\Carbon;
 use Kenyalang\Countries\Enums\CountryStatus;
 
+/**
+ * @mixin Builder
+ *
+ * @method static Builder|static query()
+ * @method static static make(array $attributes = [])
+ * @method static static create(array $attributes = [])
+ * @method static static forceCreate(array $attributes)
+ * @method Country firstOrNew(array $attributes = [], array $values = [])
+ * @method Country firstOrFail($columns = ['*'])
+ * @method Country firstOrCreate(array $attributes, array $values = [])
+ * @method Country firstOr($columns = ['*'], \Closure $callback = null)
+ * @method Country firstWhere($column, $operator = null, $value = null, $boolean = 'and')
+ * @method Country updateOrCreate(array $attributes, array $values = [])
+ * @method null|static first($columns = ['*'])
+ * @method static static findOrFail($id, $columns = ['*'])
+ * @method static static findOrNew($id, $columns = ['*'])
+ * @method static null|static find($id, $columns = ['*'])
+ *
+ * @property-read int $id
+ *
+ * @property string $name
+ * @property string $code
+ * @property string phone_code',
+ * @property string currency_code',
+ * @property string currency_name',
+ * @property string currency_symbol',
+ * @property string status',
+ * @property string timezone',
+ *
+ * @property-read Carbon $created_at
+ * @property-read Carbon $updated_at
+ *
+ * @property-read Collection|State[] $states
+ *
+ * @method Builder|static active()
+ */
 class Country extends Model
 {
     protected $fillable = [
--- src/Models/State.php
+++ src/Models/State.php
@@ -2,9 +2,41 @@
 
 namespace Kenyalang\Countries\Models;
 
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Carbon;
 use Kenyalang\Countries\Traits\HasCountry;
 
+/**
+ * @mixin Builder
+ *
+ * @method static Builder|static query()
+ * @method static static make(array $attributes = [])
+ * @method static static create(array $attributes = [])
+ * @method static static forceCreate(array $attributes)
+ * @method Country firstOrNew(array $attributes = [], array $values = [])
+ * @method Country firstOrFail($columns = ['*'])
+ * @method Country firstOrCreate(array $attributes, array $values = [])
+ * @method Country firstOr($columns = ['*'], \Closure $callback = null)
+ * @method Country firstWhere($column, $operator = null, $value = null, $boolean = 'and')
+ * @method Country updateOrCreate(array $attributes, array $values = [])
+ * @method null|static first($columns = ['*'])
+ * @method static static findOrFail($id, $columns = ['*'])
+ * @method static static findOrNew($id, $columns = ['*'])
+ * @method static null|static find($id, $columns = ['*'])
+ *
+ * @property-read int $id
+ *
+ * @property string $name
+ * @property string $code
+ * @property string latitude',
+ * @property string longitude',
+ * @property string country_name',
+ *
+ * @property-read Carbon $created_at
+ * @property-read Carbon $updated_at
+ */
 class State extends Model
 {
     use HasCountry;
@@ -12,8 +44,8 @@ class State extends Model
     protected $fillable = [
         'name',
         'code',
+        'latitude',
+        'longitude',
         'country_name',
-        'status',
-        'timezone',
     ];
 }
