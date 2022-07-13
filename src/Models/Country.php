13f5b2396cbaba28fc268f96f94186de81e1cbca
Scalia Abel <ariev@viprewards.com.my>
2022-07-13 16:29:00 +0800

    feat: add `activate` method to `Country` model

--- src/Models/Country.php
+++ src/Models/Country.php
@@ -4,6 +4,7 @@ namespace Kenyalang\Countries\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Kenyalang\Countries\Enums\CountryStatus;
 
 class Country extends Model
 {
@@ -21,6 +22,11 @@ class Country extends Model
     public function states(): HasMany
     {
         return $this->hasMany(State::class, 'country_name', 'name');
+    }
+
+    public function activate(): void
+    {
+        $this->update(['status' => CountryStatus::ACTIVE->value]);
     }
 
     public function scopeActive($query): void
