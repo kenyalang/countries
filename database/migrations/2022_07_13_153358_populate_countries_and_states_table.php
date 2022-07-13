6d72e3d20da150c52bb8cb426539b90519e5ba00
Scalia Abel <ariev@viprewards.com.my>
2022-07-13 16:16:49 +0800

    feat: add `countries` and `states` migrations

---
+++ database/migrations/2022_07_13_153358_populate_countries_and_states_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\DB;
+use Kenyalang\Countries\Models\Country;
+use Kenyalang\Countries\Models\State;
+
+return new class () extends Migration {
+    public function up()
+    {
+        $countriesFile = __DIR__ . '/json/countries+states.json';
+        $countries = json_decode(file_get_contents($countriesFile), true);
+
+        collect($countries)->each(function ($country) {
+            $timezone = Arr::first(Arr::pull($country, 'timezones'));
+            $model = Country::create([
+                'name' => $country['name'],
+                'code' => $country['iso3'],
+                'phone_code' => $country['phone_code'],
+                'currency_code' => $country['currency'],
+                'currency_name' => $country['currency_name'],
+                'currency_symbol' => $country['currency_symbol'],
+                'timezone' => $timezone['zoneName'],
+                'status' => $country['name'] === 'Malaysia' ? 'active' : 'inactive',
+            ]);
+
+            foreach ($country['states'] as $state) {
+                State::create([
+                    'name' => $state['name'],
+                    'code' => $state['state_code'],
+                    'longitude' => $state['longitude'],
+                    'latitude' => $state['latitude'],
+                    'country_name' => $model->name,
+                ]);
+            }
+        });
+    }
+
+    public function down()
+    {
+        DB::table('countries')->truncate();
+        DB::table('states')->truncate();
+    }
+};
