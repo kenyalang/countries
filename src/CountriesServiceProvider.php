40ff5aa55be0710bc1f4fedfeceb41a6b8637463
Scalia Abel <ariev@viprewards.com.my>
2022-07-15 23:21:15 +0800

    feat: enable `config` publishing and merging of `config` file

--- src/CountriesServiceProvider.php
+++ src/CountriesServiceProvider.php
@@ -9,5 +9,13 @@ class CountriesServiceProvider extends ServiceProvider
     public function boot(): void
     {
         $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
+
+        $this->mergeConfigFrom(__DIR__.'/../config/countries.php', 'countries');
+
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__.'/../config/countries.php' => config_path('countries.php'),
+            ], 'config');
+        }
     }
 }
